194474c1fa028420f07142071f65b69dd710f29d
Krishan <117340271+shoonya0@users.noreply.github.com>
2024-07-01 00:22:24 +0530

    Added a Block and Delete dialog box to the contact info.

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,16 +1,74 @@
-import { Avatar, Box, Button, Divider, IconButton, Stack, Typography } from '@mui/material'
+import React from 'react'
+import { Avatar, Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider, IconButton, Slide, Stack, Typography } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
 import { Bell, CaretRight, Phone, Prohibit, Star, Trash, VideoCamera, X } from 'phosphor-react';
-import React from 'react'
 import { useDispatch } from 'react-redux';
 import { ToggleSidebar, UpdateSidebarType } from '../redux/slice/app';
 import { faker } from '@faker-js/faker';
 import AntSwitch from './AntSwitch';
 
+
+// this gives us the slide effect when the dialog is opened
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+// functionl component to block the user
+const BlockDialog = ({ open, handleClose }) => {
+  return (
+    <Dialog open={open} TransitionComponent={Transition} keepMounted onClose={handleClose} aria-describedby="alert-dialog-slide-description" >
+      <DialogTitle>Block this contact</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-slide-description">
+          Are you sure you want to block this contact?
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleClose}>Yes</Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+// functionl component to delete the user history
+const DeleteDialog = ({ open, handleClose }) => {
+  return (
+    <Dialog open={open} TransitionComponent={Transition} keepMounted onClose={handleClose} aria-describedby="alert-dialog-slide-description" >
+      <DialogTitle>Delete this chat</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-slide-description">
+          Are you sure you want to delete this chat?
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleClose}>Yes</Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+
 const Contact = () => {
   const theme = useTheme();
 
   const dispatch = useDispatch();
+
+  // state management for the dialog
+  const [openBlock, setOpenBlock] = React.useState(false);
+
+  // state management for the delete a chat
+  const [openDelete, setOpenDelete] = React.useState(false);
+
+  // handel close fuctions
+  const handleCloseBlock = () => {
+    setOpenBlock(false);
+  }
+  const handleCloseDelete = () => {
+    setOpenDelete(false);
+  }
+
   return (
     <Box sx={{ width: 320, height: "100vh" }}>
       <Stack sx={{ height: "100%" }}>
@@ -96,15 +154,17 @@ const Contact = () => {
             </Stack>
           </Stack>
           <Stack direction={"row"} alignItems="center" spacing={2}>
-            <Button startIcon={<Prohibit />} fullWidth variant="outlined">
+            <Button onClick={() => { setOpenBlock(true) }} startIcon={<Prohibit />} fullWidth variant="outlined">
               Block
             </Button>
-            <Button startIcon={<Trash />} fullWidth variant="outlined">
+            <Button onClick={() => { setOpenDelete(true) }} startIcon={<Trash />} fullWidth variant="outlined">
               Delete
             </Button>
           </Stack>
         </Stack>
       </Stack>
+      {openBlock && <BlockDialog open={openBlock} handleClose={handleCloseBlock} />}
+      {openDelete && <DeleteDialog open={openDelete} handleClose={handleCloseDelete} />}
     </Box>
   )
 }
